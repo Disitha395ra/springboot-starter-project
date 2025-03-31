2b311e703b80e0256f2afdc25485e97b6ec50cc9
Disitha <disithar@gmail.com>
2025-03-31 23:58:27 +0530

    Add GET endpoint for retrieving a random quote

--- server/quote/src/main/java/com/quote/demo/controller/QuoteController.java
+++ server/quote/src/main/java/com/quote/demo/controller/QuoteController.java
@@ -3,6 +3,7 @@ package com.quote.demo.controller;
 import java.util.List;
 import java.util.Random;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class QuoteController {
 	        "Code, learn, repeat.",
 	        "Small steps lead to big success!"
 	);
-	
+	@GetMapping("/quote")
 	public QuoteResponse getQuote() {
 		Random random = new Random();
 		String randomQuote = quotes.get(random.nextInt(quotes.size()));
