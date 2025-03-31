2dbf62d26223ae71c9a3bf8172b0a112c7f44737
Disitha <disithar@gmail.com>
2025-03-31 23:52:36 +0530

    Add quote field and accessors to QuoteResponse model

--- server/quote/src/main/java/com/quote/demo/model/QuoteResponse.java
+++ server/quote/src/main/java/com/quote/demo/model/QuoteResponse.java
@@ -1,5 +1,17 @@
 package com.quote.demo.model;
 
 public class QuoteResponse {
-
+	private String quote;
+	
+	public QuoteResponse(String quote) {
+		this.quote = quote;
+	}
+	
+	public String getQuote() {
+		return quote;
+	}
+	
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
 }
